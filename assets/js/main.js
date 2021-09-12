5e22f3756821f7b087be85c3175dd1b6fc59fab7
yufi <yufi.gitlab@gmail.com>
2021-09-12 20:43:04 +0700

    new file

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+/* MENU SHOW */
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
+
+/* ACTIVE AND REMOVE MENU */
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/* SCROLL REVEAL ANIMATION */
+
+/* SCROLL HOME */
+
+/* SCROLL ABOUT */
