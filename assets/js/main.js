dcab627a49cbc05752ec22ef6517802e111a0ce9
yufi <yufi.gitlab@gmail.com>
2021-09-14 23:39:38 +0700

    Update 90%

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,8 +15,13 @@ showMenu('nav-toggle', 'nav-menu')
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction() {
+    // Active Link
     navLink.forEach(n => n.classList.remove('active'))
     this.classList.add('active')
+
+    // Remove menu mobile
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
